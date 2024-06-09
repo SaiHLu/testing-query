820ae6f5b6193727a2450b3632954b97fe03fe61
SaiHLu <saisailuhlaing@gmail.com>
2024-06-09 18:59:27 +0630

    fix: change db configuration

--- database.go
+++ database.go
@@ -2,13 +2,33 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 
-	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
 )
 
+func customConfig() (*pgxpool.Config, error) {
+	connString := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s", os.Getenv("DATABASE_USERNAME"), os.Getenv("DATABASE_PASSWORD"), os.Getenv("DATABASE_HOST"), os.Getenv("DATABASE_PORT"), os.Getenv("DATABASE_NAME"))
+
+	config, err := pgxpool.ParseConfig(connString)
+	if err != nil {
+		return nil, err
+	}
+
+	config.AfterConnect = func(ctx context.Context, c *pgx.Conn) error {
+		connInfo := c.ConnInfo()
+		connInfo.RegisterDefaultPgType(pgx.TextFormatCode, "text")
+		return nil
+	}
+
+	return config, nil
+}
+
 func SetupDbConnection() (*pgxpool.Pool, error) {
-	dbpool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	pgxConfig, err := customConfig()
+	dbpool, err := pgxpool.ConnectConfig(context.Background(), pgxConfig)
 	if err != nil {
 		return nil, err
 	}
